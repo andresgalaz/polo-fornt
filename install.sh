adfc76834710a7bfb4723a6b373e54e7a0092b43
Andres Galaz <andres.galaz@gmail.com>
2024-11-28 16:29:42 +0000

    install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ yarn build
 updirb build /var/www/html/react > ~/1.sh
 updirb build /var/www/html/poloplayers > ~/2.sh 
 echo Ver:
-echo vi ~/1.sh
-echo vi ~/2.sh
+echo vi ~/1.sh ~/2.sh
+vi ~/1.sh ~/2.sh
 
